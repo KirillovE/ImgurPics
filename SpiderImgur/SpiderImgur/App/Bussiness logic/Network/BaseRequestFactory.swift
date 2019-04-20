2a189e83232522aa34f3845e706e905fe577a03b
KirillovE <evgeniy.kirillov@live.ru>
2019-04-20 10:21:55 +0300

    Добавлен базовый класс для сетевых запросов

---
+++ SpiderImgur/SpiderImgur/App/Bussiness logic/Network/BaseRequestFactory.swift
@@ -0,0 +1,46 @@
+//
+//  BaseRequestFactory.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 20/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+import Alamofire
+
+/// Основной класс для работы с сетью, остальные наследуются от него
+class BaseRequestFactory: AbstractRequestFactory {
+    
+    let errorParser: AbstractErrorParser
+    let errorHandler: AbstractErrorHandler
+    let sessionManager: SessionManager    
+    let requestQueue: DispatchQueue?
+    let baseURL = URL(string: "https://api.imgur.com/3/")!
+    
+    init(errorParser: AbstractErrorParser,
+         errorHandler: AbstractErrorHandler,
+         sessionManager: SessionManager,
+         queue: DispatchQueue? = DispatchQueue.global()) {
+        self.errorParser = errorParser
+        self.errorHandler = errorHandler
+        self.sessionManager = sessionManager
+        self.requestQueue = queue
+    }
+    
+    @discardableResult
+    func request<T: Decodable>(request: URLRequestConvertible,
+                               completion: @escaping (T) -> Void) -> DataRequest {
+        return sessionManager
+            .request(request)
+            .responseCodable(errorParser: errorParser,
+                             requestQueue: requestQueue) { [weak self] (response: DataResponse<T>) in
+                                switch response.result {
+                                case .success(let decodedValue):
+                                    completion(decodedValue)
+                                case .failure(let parsedError):
+                                    self?.errorHandler.handle(parsedError)
+                                }
+        }
+    }
+    
+}
