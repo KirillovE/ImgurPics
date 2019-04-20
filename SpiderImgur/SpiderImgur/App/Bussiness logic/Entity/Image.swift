0150ecf311fa3ffea8e632a99a6a7cde77ded190
KirillovE <evgeniy.kirillov@live.ru>
2019-04-20 11:07:50 +0300

    Добавлена структура ответа сервера на запрос изображений

---
+++ SpiderImgur/SpiderImgur/App/Bussiness logic/Entity/Image.swift
@@ -0,0 +1,27 @@
+//
+//  Image.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 20/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+/// Ответ сервера на запрос
+struct ImageResponse {
+    let data: ImageData
+    let success: Bool
+    let status: Int
+}
+
+/// Информация об изображении с сервера
+struct ImageData {
+    let id: String
+    let title: String
+    let images: [Image]
+}
+
+/// Изображение, получаемое от сервера
+struct Image: Decodable {
+    let id: String
+    let link: String
+}
