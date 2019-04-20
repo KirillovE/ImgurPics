5ce6f24b1328a94bdda79afd23b89926150453eb
KirillovE <evgeniy.kirillov@live.ru>
2019-04-20 18:38:01 +0300

    Добавлен протокол для создания фабрики запросов в сеть

---
+++ SpiderImgur/SpiderImgur/App/Bussiness logic/Network/RequestsFactoryMaker.swift
@@ -0,0 +1,25 @@
+//
+//  RequestsFactoryMaker.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 20/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+/// Создатель фабрики запросов в сеть
+protocol RequestsFactoryMaker {
+    
+    /// Скачивание изображений
+    ///
+    /// - Parameters:
+    ///   - page: Номер страницы с изображениями
+    ///   - completion: Завершающий блок с полученными изображениями
+    func fetchImages(page: Int, completion: @escaping ([Image]) -> Void)
+    
+    /// Скачивание комментариев для изображения или галереи
+    ///
+    /// - Parameters:
+    ///   - imageID: Идентификатор изображения или галереи
+    ///   - comletion: Завершающий блок с полученными комментариями
+    func fetchComments(forImage imageID: Int, comletion: @escaping ([Comment]) -> Void)
+}
