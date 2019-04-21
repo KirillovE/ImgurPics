feae06b2557bd0973eb6110c7c3073957cb32d5a
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 14:51:01 +0300

    Настроено отображение коллекции

--- SpiderImgur/SpiderImgur/App/Flow/Images screen/ImagesView.swift
+++ SpiderImgur/SpiderImgur/App/Flow/Images screen/ImagesView.swift
@@ -11,5 +11,24 @@ import UIKit
 final class ImagesView: UIView {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    private let reuseID = "ImagesCell"
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureCollection()
+    }
+    
+    private func configureCollection() {
+        collectionView.register(UINib(nibName: "ImageCollectionCell", bundle: nil), forCellWithReuseIdentifier: reuseID)
+        
+        let spacing = CGFloat(11)
+        let layout = UICollectionViewFlowLayout()
+        let collectionWidth = collectionView.frame.width
+        let itemWidth = collectionWidth / 2 - spacing / 2
+        let itemHeight = itemWidth + 46
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+    }
+    
 }
