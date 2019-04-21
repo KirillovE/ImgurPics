a30e6222a0220aca03f98a49ea81e246b693732c
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 14:30:48 +0300

    Представление контроллера вынесено в отдельный класс

---
+++ SpiderImgur/SpiderImgur/App/Flow/Comments screen/CommentsView.swift
@@ -0,0 +1,15 @@
+//
+//  CommentsView.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 21/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+import UIKit
+
+final class CommentsView: UIView {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+}
