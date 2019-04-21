b012f1862e96e28a9c8361e6120a5632752e1ff1
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 21:07:28 +0300

    Добавлены протоколы для работы с детальной информацией об изображении

---
+++ SpiderImgur/SpiderImgur/App/Flow/Details screen/DetailProtocols.swift
@@ -0,0 +1,27 @@
+//
+//  DetailProtocols.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 21/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+/// Источник данных в виде детальной информации об изображении
+protocol DetailsDataSource: AnyObject {
+    
+    /// Массив комментариев для отображения в таблице или коллекции
+    var comments: [Comment] { get set }
+    
+    /// Подробности об изображении
+    var imageDetails: [String: String] { get set }
+}
+
+/// Определяет правила для работы с детальной информацией об изображении
+protocol DetailsList: AnyObject {
+    
+    /// Источник данных для таблицы или коллекции
+    var listDataSource: DetailsDataSource? { get set }
+    
+    /// Обновление таблицы или коллекции
+    func update()
+}
