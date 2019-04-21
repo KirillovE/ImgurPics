aa19b48b02271e28d62fab736f28d7fc12a8cecb
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 09:24:03 +0300

    Экранам назначены контроллеры

---
+++ SpiderImgur/SpiderImgur/App/Flow/CommentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CommentsViewController.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 21/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SpiderImgur/SpiderImgur/App/Flow/ImagesViewController.swift
+++ SpiderImgur/SpiderImgur/App/Flow/ImagesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImagesViewController.swift
 //  SpiderImgur
 //
 //  Created by Евгений Кириллов on 20/04/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ImagesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
