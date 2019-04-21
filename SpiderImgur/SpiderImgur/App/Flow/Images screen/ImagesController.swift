cdfd8820ade29db7dc760d2bd4fc202cef4e3100
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 16:37:24 +0300

    Добавлена загрузка новых изображений по мере прокрутки

--- SpiderImgur/SpiderImgur/App/Flow/Images screen/ImagesController.swift
+++ SpiderImgur/SpiderImgur/App/Flow/Images screen/ImagesController.swift
@@ -11,6 +11,7 @@ import UIKit
 final class ImagesController: UIViewController, ImagesDataSource {
     
     var images = [ImageViewModel]()
+    private var nextPage = 0
     private let requestFactory = FactoryAssembler().assembleRequestFactory()
     private var customView: ImageList? {
         return viewIfLoaded as? ImageList
@@ -23,9 +24,13 @@ final class ImagesController: UIViewController, ImagesDataSource {
     }
     
     private func loadImages() {
-        requestFactory.fetchImages(page: 0) { [weak self] imageResponse in
-            self?.images = imageResponse.images
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        requestFactory.fetchImages(page: nextPage) { [weak self] imageResponse in
+            self?.images.append(contentsOf: imageResponse.images)
             self?.customView?.update()
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
         }
     }
 
@@ -35,6 +40,14 @@ extension ImagesController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         performSegue(withIdentifier: "showComments", sender: nil)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let imagesBeforeLoadingNext = images.count - 15
+        if indexPath.row == imagesBeforeLoadingNext {
+            nextPage += 1
+            loadImages()
+        }
     }
     
 }
