c45fb3a4d1e9f631ac5bc4b60dd89434c8a7a852
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 17:48:41 +0300

    Добавлен словарь для заполнения детальной информации об изображении

--- SpiderImgur/SpiderImgur/App/Flow/Details screen/DetailsController.swift
+++ SpiderImgur/SpiderImgur/App/Flow/Details screen/DetailsController.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 final class DetailsController: UIViewController {
-
+    
+    /// Изображение для показа детальной информации
+    var imageViewModel: ImageViewModel? {
+        didSet {
+            guard let model = imageViewModel else { return }
+            details["Название"] = model.title
+            details["Количество просмотров"] = String(model.views)
+            details["Количество добавлений в избранное"] = String(model.favoriteCount)
+            details["Количество комментариев"] = String(model.commentCount)
+            
+            guard let description = model.description else { return  }
+            details["Описание"] = description
+        }
+    }
+    
+    private var details: [String: String] = [:]
 }
