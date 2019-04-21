4df50f9b04cf03e7cb06b5205cf2436f2dee0ea7
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 11:27:50 +0300

    Добавлен класс для работы с представлением экрана изображений

---
+++ SpiderImgur/SpiderImgur/App/Flow/Images screen/ImagesView.swift
@@ -0,0 +1,22 @@
+//
+//  ImagesView.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 21/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+import UIKit
+
+class ImagesView: UIView {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
