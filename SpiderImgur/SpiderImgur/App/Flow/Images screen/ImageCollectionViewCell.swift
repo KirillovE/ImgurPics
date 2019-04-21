00fd6aa730554bb4c2ec831a2d64044935c6ee80
KirillovE <evgeniy.kirillov@live.ru>
2019-04-21 09:34:25 +0300

    Добавлен класс для ячейки коллекции

---
+++ SpiderImgur/SpiderImgur/App/Flow/Images screen/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  SpiderImgur
+//
+//  Created by Евгений Кириллов on 21/04/2019.
+//  Copyright © 2019 Триада. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
